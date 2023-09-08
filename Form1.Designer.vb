2bca9afdcaf47a2c9d774ec016e9927df88634a5
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-08 11:53:44 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(150, 107)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Phone Number"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(344, 105)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(100, 26)
+        Me.txtPhone.TabIndex = 1
+        '
+        'Btn1
+        '
+        Me.Btn1.Location = New System.Drawing.Point(263, 215)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(181, 51)
+        Me.Btn1.TabIndex = 2
+        Me.Btn1.Text = "Show Number"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.txtPhone)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPhone As TextBox
+    Friend WithEvents Btn1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        Dim phonenumber As String
+
+        phonenumber = txtPhone.Text
+
+        If IsNumeric(txtPhone.Text) Then
+            MsgBox("call " & phonenumber)
+
+        Else
+            MsgBox(" enter number only!")
+
+
+        End If
+
+
+
+    End Sub
+End Class
